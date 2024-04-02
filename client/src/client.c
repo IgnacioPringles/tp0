80ed7f0d0836415ad86bef2b5f370ef176c804cc
Ignacio Pringles <ipringles@frba.utn.edu.ar>
2024-04-02 10:47:02 -0300

    Implementación paquete

--- client/src/client.c
+++ client/src/client.c
@@ -108,13 +108,21 @@ void paquete(int conexion)
 {
 	// Ahora toca lo divertido!
 	char* leido;
-	t_paquete* paquete;
+	t_paquete* paquete = crear_paquete();
 
 	// Leemos y esta vez agregamos las lineas al paquete
+	leido = readline("> ");
+	while (strcmp(leido, "") != 0) {
+		agregar_a_paquete(paquete, leido, strlen(leido) + 1);
+		free(leido);
+		leido = readline("> ");
+	}
 
+	enviar_paquete(paquete, conexion);
 
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
-	
+	free(leido);
+	eliminar_paquete(paquete);
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
