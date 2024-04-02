b3dc4b4612fc8560c38972250819d5822d9d9570
Ignacio Pringles <ipringles@frba.utn.edu.ar>
2024-04-02 10:47:13 -0300

    Implementación terminar_programa

--- client/src/client.c
+++ client/src/client.c
@@ -129,4 +129,6 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	log_destroy(logger);
+	config_destroy(config);
 }
